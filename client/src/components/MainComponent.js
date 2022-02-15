bfb269c48ab84a4f490a46b5fdd77eb6490949d6
sanjay <san692001@gmail.com>
2022-02-15 20:33:56 +0530

    assignment 4

--- client/src/components/MainComponent.js
+++ client/src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Contact from './ContactComponent';
 import About from './AboutusComponent';
 import { Switch , Route , Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { postComment , addComment , fetchComments, fetchDishes, fetchPromos,  fetchLeaders } from '../redux/ActionCreators';
+import { postComment , addComment , fetchComments, fetchDishes, fetchPromos,  fetchLeaders, postFeedback } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form'
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
@@ -28,7 +28,8 @@ const mapDispatchToProps = dispatch => ({
   fetchComments: () => dispatch(fetchComments()),
   fetchPromos: () => dispatch(fetchPromos()),
   postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
-  fetchLeaders: () => dispatch(fetchLeaders())
+  fetchLeaders: () => dispatch(fetchLeaders()),
+  postFeedback: (firstname, lastname, telnum, email, agree, contactType, message) => dispatch(postFeedback(firstname, lastname, telnum, email, agree, contactType, message))
 });
 
 
@@ -90,7 +91,7 @@ class Main extends Component {
                   <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
                   <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
                   <Route path='/menu/:dishId' component={DishWithId} />
-                  <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+                  <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} postFeedback={this.props.postFeedback} />} />
                   <Redirect to="/home" />
               </Switch>
             </CSSTransition>
--- client/src/redux/ActionCreators.js
+++ client/src/redux/ActionCreators.js
@@ -182,3 +182,52 @@ export const addLeaders = (leaders) => ({
   payload: leaders
 });
 
+
+export const postFeedback = (firstname, lastname, telnum, email, agree, contactType, message) => () => {
+
+  const newFeedback = {
+      firstname: firstname,
+      lastname: lastname,
+      telnum: telnum,
+      email: email,
+      agree: agree,
+      contactType: contactType,
+      message: message,
+  }
+  newFeedback.date = new Date().toISOString();
+
+  return fetch(baseUrl + 'feedback', {
+      method: 'POST',
+      body: JSON.stringify(newFeedback),
+      headers: {
+          'Content-type': 'application/json'
+      },
+      credentials: 'same-origin'
+  })
+      .then(response => {
+
+          if (response.ok) {
+
+              return response;
+          }
+          else {
+              var error = new Error('Error ' + response.status + ': ' + response.statusText)
+              error.response = response;
+              throw error;
+          }
+      },
+          ////if no responmse from server
+          error => {
+              var errmess = new Error(error.message);
+              throw errmess;
+          }
+      )
+      .then(response => response.json())
+      .then(response => alert("Thank you for your feedback!"+JSON.stringify(response)))
+      .catch(error => {
+          console.log('Post Comments', error.message);
+          alert('Comment could not be posted\nError' + error.message);
+      });
+
+}
+
